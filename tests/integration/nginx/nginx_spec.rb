8458a631a5bac87ddfb0623d013edacbd0ef07b3
Chris Aumann <me@chr4.org>
2020-06-17 15:51:14 +0200

    Add integration tests via Github actions

---
+++ tests/integration/nginx/nginx_spec.rb
@@ -0,0 +1,26 @@
+control 'nginx' do
+  title 'should be installed & configured'
+
+  describe file('/etc/apt/sources.list.d/nginx.list') do
+    its('content') { should match /^deb http:\/\/nginx.org\/packages\/mainline\/ubuntu / }
+  end
+
+  describe package('nginx') do
+    it { should be_installed }
+    its('version') { should cmp >= '1.17.10' }
+  end
+
+  describe file('/etc/nginx/nginx.conf') do
+    its('mode') { should cmp '0644' }
+    its('content') { should match /^\s*worker_processes/ }
+  end
+
+  describe file('/etc/nginx/conf.d/default.conf') do
+    it { should_not exist }
+  end
+
+  describe service('nginx') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
---
+++ tests/top.sls
@@ -0,0 +1,4 @@
+base:
+  '*':
+    - nginx
+    - nginx.unit.php
